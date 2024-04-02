5dfecb5a542078858ca2850bf5b907634f0d5b49
christoph <63956995+christoph139@users.noreply.github.com>
2024-04-02 15:24:39 +0200

    .

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-# save this as app.py
 from flask import Flask, request
 from markupsafe import escape
 
@@ -8,3 +7,6 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+if __name__ == '__main__':
+    app.run()
